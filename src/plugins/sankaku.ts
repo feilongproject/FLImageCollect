88140c2646e65779c02dd43c9a6db2bd104f2169
feilongproject <flzs6137@gmail.com>
2022-10-01 00:48:51 +0800

    feat: 增加simple的log显示

--- src/plugins/sankaku.ts
+++ src/plugins/sankaku.ts
@@ -9,32 +9,34 @@ import config from "../../config/config.json";
 const MOVE_LEFT = Buffer.from('1b5b3130303044', 'hex').toString();
 const CLEAR_LINE = Buffer.from('1b5b304b', 'hex').toString();
 const MOVE_UP = Buffer.from('1b5b3141', 'hex').toString();
+const options = {
+    timesLimit: 1,
+    unlimited: false,
+    verify: false,
+    logLevel: "full",
+};
 
-var lastThreadLen = 0;
 var threadsQueue: ThreadInfo[] = [];
+var lastThreadLen = 0;
+var nowPage = ``;
+var nowSearch = ``;
 
 export async function downloadTags(keywords: string[]) {
-    const keyword = keywords[0];
+    nowSearch = keywords[0];
     var nextPage: string = ``;
     var rlBreak = false;
 
-    if (!keyword) {
+    if (!nowSearch) {
         log.error(`未找到关键词，请重试！`);
         return;
     }
 
-    const options = {
-        timesLimit: 1,
-        unlimited: false,
-
-    };
     for (let i = 1; i < keywords.length; i += 2) {
         const argvC = keywords[i];
         const argvD = keywords[i + 1];
         switch (argvC) {
             case "-t":
             case "--times":
-
                 if (argvD == "-1") {
                     options.unlimited = true;
                     continue;
@@ -45,20 +47,39 @@ export async function downloadTags(keywords: string[]) {
                     return;
                 }
                 break;
+
             case "-n":
             case "--next":
                 nextPage = argvD;
                 log.info(`已设置续传页面为：${nextPage}`);
                 break;
+
+            case "-v":
+            case "--verify":
+                i--;
+                options.verify = true;
+                break;
+
+            case "-l":
+            case "--log":
+                if (/(simple|full)/.test(argvD)) {
+                    options.logLevel = /(simple|full)/.exec(argvD)[1];
+                    log.info(`已设置log等级为：${options.logLevel}`);
+                } else {
+                    log.error(`错误的日志等级！使用simple或者full进行日志输出！`);
+                    return;
+                }
+                break;
+
             default:
                 break;
         }
     }
 
-    log.info(`正在搜索关键词：\x1b[36m${keyword}\x1b[0m，搜索次数：\x1b[36m${options.unlimited ? "无限制" : `${options.timesLimit}`}\x1b[0m`);
-    log.mark('开始获取资源并下载中，按任意键停止下载！');
+    log.info(`正在搜索关键词：\x1b[36m${nowSearch}\x1b[0m，搜索次数：\x1b[36m${options.unlimited ? "无限制" : `${options.timesLimit}`}\x1b[0m`);
+    log.info('开始获取资源并下载中，按任意键停止下载！');
 
-    const a = process.stdin.once("data", () => {
+    process.stdin.once("data", () => {
         //process.stdout.write('\x1B[0K');
         //process.stdout.write(data.toString());
         process.stdout.write(`\n\x1B[0K已停止\n`);
@@ -71,9 +92,9 @@ export async function downloadTags(keywords: string[]) {
         lastThreadLen = 0;//清空历史线程
 
         var threadsInfoStr = JSON.stringify(threadsQueue);
-        const data = await sankakuSearch(keyword, nextPage);
+        const data = await sankakuSearch(nowSearch, nextPage);
 
-        log.info(`G(${keyword}):${nextPage}`);
+        if (options.logLevel != "simple") log.info(`G(${nowSearch}):${nextPage}`);
 
         if (!data || !data.meta.next || !data.data) {
             log.error(`页面获取出错！`, data);
@@ -85,7 +106,7 @@ export async function downloadTags(keywords: string[]) {
             log.info(`未找到结束页面，停止搜索`);
             return;
         }
-        const nowPage = nextPage;
+        nowPage = nextPage;
         nextPage = data.meta.next;
 
         await redis.hSet("turnIndex", (nowPage || "index"), nextPage);
@@ -117,6 +138,20 @@ export async function downloadTags(keywords: string[]) {
                 });
             }
 
+            if (!page.file_url) {
+                const threadId = createThreadId({
+                    pid: page.id,
+                    percent: 100,
+                    sourceMD5: page.md5,
+                    err: `未找到文件url`,
+                });
+                getThreadStatus(threadId);
+                continue;
+            }//如果未找到则push错误信息，并继续循环
+
+            const fileName = `${page.id}${path.extname(page.file_url.match(/.*(?=\?)/)[0])}`;//取得文件名称（id+后缀）
+            const filePath = `${_path}/${config.downloadFiles}/${fileName}`;//取得文件绝对路径（路径+文件名称）
+
             await picRedis.hSet(`pid:${page.id}`, [
                 ["id", page.id],
                 ["md5", page.md5],
@@ -133,21 +168,10 @@ export async function downloadTags(keywords: string[]) {
                 ["vote_count", page.vote_count],
                 ["fav_count", page.fav_count],
                 ["==>father", nowPage],
+                ["==>fileName", fileName],
+                ["==>filePath", filePath],
             ]);//扔进数据库
 
-            if (!page.file_url) {
-                const threadId = createThreadId({
-                    pid: page.id,
-                    percent: 100,
-                    sourceMD5: page.md5,
-                    err: `未找到文件url`,
-                });
-                getThreadStatus(threadId);
-                continue;
-            }//如果未找到则push错误信息，并继续循环
-
-            const fileName = `${page.id}${path.extname(page.file_url.match(/.*(?=\?)/)[0])}`;//取得文件名称（id+后缀）
-            const filePath = `${_path}/${config.downloadFiles}/${fileName}`;//取得文件绝对路径（路径+文件名称）
             if (fs.existsSync(filePath)) {
                 //log.info(`id:${page.id}已下载`);
                 const threadId = createThreadId({
@@ -204,7 +228,7 @@ export async function downloadTags(keywords: string[]) {
                 if (threadInfo.percent == 100) threadEndLen++;
             }
             if (threadEndLen == threadsQueue.length) {
-                log.info(`已完成当前所有线程！`);
+                if (options.logLevel != "simple") log.info(`已完成当前所有线程！`);
                 break;
             }
             log.error(`${MOVE_UP}${MOVE_LEFT}${CLEAR_LINE}似乎有${threadsQueue.length - threadEndLen}个线程卡死\n`);
@@ -256,31 +280,44 @@ function getThreadStatus(activeId: number, anotherLen = 0) {
 
     var etc = 0;
     const lineFill = Array.from(Array(lastThreadLen == 0 ? 0 : lastThreadLen + 1 + anotherLen), () => MOVE_UP);
-    //if (lastThreadLen > 0)
-    process.stdout.write(`${MOVE_LEFT}${CLEAR_LINE}${lineFill.join("")}\x1B[K${lineFill.length}==========下载线程列表==========\n`);
 
-    for (const [index, thread] of threadsQueue.entries()) {
-        if (index + 6 >= process.stdout.rows) {
-            etc++;
-            process.stdout.write(`${MOVE_UP}${MOVE_LEFT}${CLEAR_LINE}省略${index - process.stdout.rows + 6}线程...\n`);
-            continue;
+    if (options.logLevel == "simple") {
+        var totalPercent = 0;
+        process.stdout.write(`${(lineFill.length || anotherLen) ? MOVE_UP : ""}${MOVE_LEFT}${CLEAR_LINE}`);
+
+        for (const thread of threadsQueue) {
+            if (thread.err) process.stdout.write(`\x1B[41;30m${thread.err}\x1B[m\n`);
+            else totalPercent += thread.percent;
+
         }
-        process.stdout.write('\x1B[K');
-        process.stdout.write(`(活动线程:${activeId})线程${thread.threadId}--图片id：${thread.pid}，进度：`);
-        if (thread.err) {
-            process.stdout.write(`\x1B[41;30m${thread.err}\x1B[m`);
-        } else if (thread.info) {
-            process.stdout.write(`\x1B[42;30m${thread.info}\x1B[m`);
-        } else if (thread.percent == -1) {
-            process.stdout.write(`\x1B[41;30m文件无效，当前总线程重启后重试\x1B[m`);
-        } else if (thread.percent == 100) {
-            process.stdout.write(`\x1B[42;30m下载完成，线程结束\x1B[m`);
-        } else {
-            process.stdout.write(`\x1B[43;30m${thread.percent.toFixed(2)}%\x1B[m`);
+        process.stdout.write(`${lineFill.length}(活动线程:${activeId})，当前进度：${(totalPercent / threadsQueue.length).toFixed(2)}%，G(${nowSearch}):${nowPage}\n`);
+
+    } else {
+
+        process.stdout.write(`${MOVE_LEFT}${CLEAR_LINE}${lineFill.join("")}\x1B[K${lineFill.length}==========下载线程列表==========\n`);
+
+        for (const [index, thread] of threadsQueue.entries()) {
+            if (index + 6 >= process.stdout.rows) {
+                etc++;
+                process.stdout.write(`${MOVE_UP}${MOVE_LEFT}${CLEAR_LINE}省略${index - process.stdout.rows + 6}线程...\n`);
+                continue;
+            }
+            process.stdout.write('\x1B[K');
+            process.stdout.write(`(活动线程:${activeId})线程${thread.threadId}--图片id：${thread.pid}，进度：`);
+            if (thread.err) {
+                process.stdout.write(`\x1B[41;30m${thread.err}\x1B[m`);
+            } else if (thread.info) {
+                process.stdout.write(`\x1B[42;30m${thread.info}\x1B[m`);
+            } else if (thread.percent == -1) {
+                process.stdout.write(`\x1B[41;30m文件无效，当前总线程重启后重试\x1B[m`);
+            } else if (thread.percent == 100) {
+                process.stdout.write(`\x1B[42;30m下载完成，线程结束\x1B[m`);
+            } else {
+                process.stdout.write(`\x1B[43;30m${thread.percent.toFixed(2)}%\x1B[m`);
+            }
+            process.stdout.write(`\n`);
         }
-        process.stdout.write(`\n`);
     }
-
     lastThreadLen = threadsQueue.length - etc;
 }
 
